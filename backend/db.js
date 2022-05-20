ad49b5ff08c274ee7ea44ea7f8e0ee7f776d131e
azharjustin <azharjustin37@gmail.com>
2022-05-20 16:41:19 +0530

    solved

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,7 @@
 const mongoose = require("mongoose");
 
 // const url=process.env.url;
-const url =
-  "mongodb://stackdata:welcome123@cluster0-shard-00-00.b68sv.mongodb.net:27017,cluster0-shard-00-01.b68sv.mongodb.net:27017,cluster0-shard-00-02.b68sv.mongodb.net:27017/stackoverflow?authSource=admin&replicaSet=atlas-12h1ih-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+
   
   module.exports.connect = () => {
   mongoose
